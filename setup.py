816b0449f66ebc9591a5ce21a9f92fab0c327b26
Josh Barber <j.barber501@gmail.com>
2018-02-11 17:10:47 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='python-btcmarkets',
+      version='0.1',
+      description='Python3 Wrapper for the BTCMarkets APIs',
+      url='https://github.com/SigmaAdvancedAnalytics/python-btcmarkets',
+      author='Joshua Barber',
+      author_email='contact@jbarber.info',
+      license='MIT',
+      packages=['python-btcmarkets'],
+      zip_safe=False)
